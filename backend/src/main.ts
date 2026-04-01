23d01364d7bca5ed9177ac92c5c19bb78b8fb192
Agustin Genovese <agusgenot@gmail.com>
2026-04-01 17:54:22 -0300

    d

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,22 @@
-import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`Backend corriendo en http://localhost:${port}`);
+  
+  // Si estamos en local, corremos normal
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(process.env.PORT ?? 3000);
+  }
+  
+  return app;
 }
 
-bootstrap();
+// ESTA PARTE ES CRUCIAL PARA VERCEL
+export default async (req: any, res: any) => {
+  const app = await bootstrap();
+  await app.init();
+  const instance = app.getHttpAdapter().getInstance();
+  instance(req, res);
+};
